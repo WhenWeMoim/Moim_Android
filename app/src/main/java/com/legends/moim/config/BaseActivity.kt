a1c9d8f78266d73092a1c99c084119950116e0e3
zae-hyeong <p0138@naver.com>
2022-03-29 08:49:48 +0900

    apply MVVP Model

---
+++ app/src/main/java/com/legends/moim/config/BaseActivity.kt
@@ -0,0 +1,29 @@
+package com.legends.moim.config
+
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+open class BaseActivity: AppCompatActivity(), View.OnClickListener, BaseDialog.BaseDialogClickListener {
+    override fun onClick(v: View?) {
+
+    }
+
+    override fun onOKClicked() {
+
+    }
+
+//    override fun onCancelClicked() {
+//    }
+
+    fun showDialog(title: String, message: String , okMessage: String) {
+        val dig = BaseDialog(this)
+        dig.listener = this
+        dig.show(title, message, okMessage)
+    }
+
+    fun showImgDialog(title: String, message: String , okMessage: String, img : Int) {
+        val dig = BaseDialog(this)
+        dig.listener = this
+        dig.show(title, message, okMessage, img)
+    }
+}
---
+++ app/src/main/java/com/legends/moim/config/BaseFragment.kt
@@ -0,0 +1,18 @@
+package com.legends.moim.config
+
+import android.view.View
+import androidx.fragment.app.Fragment
+
+open class BaseFragment: Fragment(), View.OnClickListener, BaseDialog.BaseDialogClickListener {
+
+    override fun onClick(v: View?) {
+
+    }
+
+    override fun onOKClicked() {
+
+    }
+
+//    override fun onCancelClicked() {
+//    }
+}
