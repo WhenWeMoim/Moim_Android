6bdb9ac03e021ab43d4461c61b28428374e03423
zae-hyeong <p0138@naver.com>
2022-04-11 22:50:44 +0900

    [feat] dialog time, date

---
+++ app/src/main/java/com/legends/moim/src/makeMoim/DateDialog.kt
@@ -0,0 +1,86 @@
+package com.legends.moim.src.makeMoim
+
+import android.app.Dialog
+import android.content.Context
+import android.view.Window
+import android.widget.ImageView
+import android.widget.TextView
+import com.google.android.material.datepicker.MaterialCalendar
+import com.legends.moim.R
+
+class DateDialog(context : Context) {
+
+    val dialog = Dialog(context)
+
+    private lateinit var tvTitle: TextView
+    private lateinit var tvMessage: TextView
+    private lateinit var btnOK: TextView
+
+    var listener: BaseDialogClickListener? = null
+
+    open fun show(title: String, message: String, okMessage: String) {
+        dialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setContentView(R.layout.dialog_base)
+        dialog.setCancelable(false)
+
+        tvTitle = dialog.findViewById(R.id.dialog_base_title_tv)
+        tvTitle.text = title
+
+        tvMessage = dialog.findViewById(R.id.dialog_base_message_tv)
+        tvMessage.text = message
+
+        btnOK = dialog.findViewById(R.id.dialog_base_ok_btn_tv)
+        btnOK.text = okMessage
+        btnOK.setOnClickListener {
+
+            listener!!.onOKClicked()
+
+            dialog.dismiss()
+        }
+
+//        btnCancel = dialog.findViewById(R.id.dialog_base_close_btn_iv)
+//        btnCancel.setOnClickListener {
+//            listener!!.onCancelClicked()
+//            dialog.dismiss()
+//        }
+
+        dialog.show()
+    }
+
+    open fun show(title: String, message: String, okMessage: String, img: Int) {
+        dialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setContentView(R.layout.dialog_base)
+        dialog.setCancelable(false)
+
+        tvTitle = dialog.findViewById(R.id.dialog_base_title_tv)
+        tvTitle.text = title
+
+        tvMessage = dialog.findViewById(R.id.dialog_base_message_tv)
+        tvMessage.text = message
+        tvMessage.setCompoundDrawablesRelativeWithIntrinsicBounds(0, img, 0, 0)
+
+        btnOK = dialog.findViewById(R.id.dialog_base_ok_btn_tv)
+        btnOK.text = okMessage
+        btnOK.setOnClickListener {
+
+            listener!!.onOKClicked()
+
+            dialog.dismiss()
+        }
+
+//        btnCancel = dialog.findViewById(R.id.dialog_base_cancel_btn_iv)
+//        btnCancel.setOnClickListener {
+//            listener!!.onCancelClicked()
+//            dialog.dismiss()
+//        }
+
+        dialog.show()
+    }
+
+    interface BaseDialogClickListener {
+        fun onOKClicked()
+//        fun onCancelClicked()
+    }
+}
--- app/src/main/java/com/legends/moim/src/makeMoim/MakeMoimActivity.kt
+++ app/src/main/java/com/legends/moim/src/makeMoim/MakeMoimActivity.kt
@@ -1,6 +1,46 @@
 package com.legends.moim.src.makeMoim
 
+import android.os.Bundle
+import android.view.View
+import com.legends.moim.R
 import com.legends.moim.config.BaseActivity
+import com.legends.moim.databinding.ActivityMakeMoimBinding
 
 class MakeMoimActivity: BaseActivity() {
+
+    lateinit var binding : ActivityMakeMoimBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMakeMoimBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initView()
+    }
+
+    fun initView() {
+        binding.makeMoimSelectDateBtn.setOnClickListener(this)
+        binding.makeMoimSelectTimeBtn.setOnClickListener(this)
+        binding.makeMoimSettingTv.setOnClickListener(this)
+        binding.makeMoimCompleteBtn.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        super.onClick(v)
+        when(v!!.id) {
+            R.id.make_moim_select_date_btn -> {
+
+            }
+            R.id.make_moim_select_time_btn -> {
+
+            }
+            R.id.make_moim_setting_tv -> {
+
+            }
+            R.id.make_moim_complete_btn -> {
+
+            }
+
+        }
+    }
 }
