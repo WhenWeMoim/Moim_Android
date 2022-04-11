523f28f6d8feb98a7b6a7eb086c75c7d71e6d20c
zae-hyeong <p0138@naver.com>
2022-04-11 22:41:08 +0900

    commit before merge

--- app/src/main/java/com/legends/moim/src/home/MainActivity.kt
+++ app/src/main/java/com/legends/moim/src/home/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.legends.moim.src
+package com.legends.moim.src.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
--- app/src/main/java/com/legends/moim/src/splash/SplashActivity.kt
+++ app/src/main/java/com/legends/moim/src/splash/SplashActivity.kt
@@ -1,4 +1,10 @@
 package com.legends.moim.src.splash
 
+import android.content.Intent
+import android.os.Handler
+import android.os.Looper
+import androidx.core.content.ContextCompat.startActivity
+import com.legends.moim.src.home.MainActivity
+
 class SplashActivity {
 }
