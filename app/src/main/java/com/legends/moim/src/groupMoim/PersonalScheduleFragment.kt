16fb3ff73ae8566cd0d4552f2c8e09551f579e87
zae-hyeong <p0138@naver.com>
2022-06-02 23:50:45 +0900

    [fix, feat] resetSchedule, scheduleFragment

--- app/src/main/java/com/legends/moim/src/groupMoim/PersonalScheduleFragment.kt
+++ app/src/main/java/com/legends/moim/src/groupMoim/PersonalScheduleFragment.kt
@@ -1,14 +1,14 @@
 package com.legends.moim.src.groupMoim
 
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.TableLayout
-import android.widget.TableRow
-import android.widget.Toast
+import android.widget.*
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.legends.moim.R
 import com.legends.moim.databinding.FragmentSchedulePersonalBinding
@@ -21,12 +21,13 @@ import com.legends.moim.databinding.FragmentSchedulePersonalBinding
 
 class PersonalScheduleFragment: Fragment() {
 
-    private lateinit var binding: FragmentSchedulePersonalBinding
-
     private var numOfDays = 7 //임시데이터 -> todo 행 수 = 선택한 날짜 개수 makingMoim.Date.size
     private var numOfTimes = 12 //임시데이터 -> todo makingMoim.endTimeHour - makingMoim.startTimeHour //열 수 = 시간 구간 개수
 
+    private lateinit var dateLayout: LinearLayout
+    private lateinit var timeLayout: LinearLayout
     private lateinit var scheduleLayout: TableLayout
+
     private lateinit var timeRows: Array<TableRow> //한개 행(row)
     private lateinit var scheduleButtons: Array<Array<Button>>
 
@@ -35,7 +36,6 @@ class PersonalScheduleFragment: Fragment() {
     override fun onCreateView( inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         super.onCreateView(inflater, container, savedInstanceState)
-        binding = FragmentSchedulePersonalBinding.inflate(layoutInflater)
         val v: View = inflater.inflate(R.layout.fragment_schedule_personal, container, false)
 
         getData()
@@ -63,6 +63,67 @@ class PersonalScheduleFragment: Fragment() {
     }
 
     private fun initScheduleTable(v: View) {
+
+        //time 시간표 삽입
+        timeLayout = v.findViewById(R.id.personal_schedule_time_linearLayout) as LinearLayout
+
+        val timeTextPm = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, 0, 1F)
+
+        lateinit var timeText: TextView
+
+        var i: Int = 0
+        while( i <= numOfTimes ) {
+            timeText = TextView(context)
+            timeText.layoutParams = timeTextPm
+            timeText.text = String.format("%d시", thisMoim.startTimeHour + i )
+            timeText.textSize = 10F
+            timeText.setTextColor(
+                ContextCompat.getColor(
+                    requireActivity().applicationContext,
+                    R.color.black
+                )
+            )
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                timeText.typeface = resources.getFont(R.font.notosans_kr_regular)
+            }
+            timeText.includeFontPadding = false
+
+            timeLayout.addView(timeText)
+
+            i++
+        }
+
+        //date 시간표 삽입
+        dateLayout = v.findViewById(R.id.personal_schedule_date_linearLayout) as LinearLayout
+
+        val dateTextPm = LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.WRAP_CONTENT, 1F)
+        dateTextPm.gravity = Gravity.CENTER
+        lateinit var dateText: TextView
+
+        i = 0
+        while( i < numOfDays ) {
+            dateText = TextView(context)
+            dateText.layoutParams = dateTextPm
+            dateText.text = String.format("%d월\n%d일\n(%c)", thisMoim.dates[i].month, thisMoim.dates[i].day, thisMoim.dates[i].dayOfWeek)
+
+            dateText.setTextColor(
+                ContextCompat.getColor(
+                    requireActivity().applicationContext,
+                    R.color.moim_main_1
+                )
+            )
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                dateText.typeface = resources.getFont(R.font.notosans_kr_regular)
+            }
+            dateText.includeFontPadding = false
+            dateText.gravity = Gravity.CENTER
+            dateText.textSize = 10F
+
+            dateLayout.addView(dateText)
+
+            i++
+        }
+
         scheduleLayout = v.findViewById(R.id.personal_schedule_tableLayout) as TableLayout
 
         timeRows = Array<TableRow>(size = numOfTimes, init = { TableRow(v.context) } ) //한개 행 = 날짜 개수
@@ -70,11 +131,11 @@ class PersonalScheduleFragment: Fragment() {
             init = { Array<Button>(size = numOfDays, init = {Button(v.context)} ) }
         )
 
-        val rowPm: TableLayout.LayoutParams = TableLayout.LayoutParams(0, 0, 1F)
+        val rowPm: TableLayout.LayoutParams = TableLayout.LayoutParams(0, 40, 1F)
         val cellPm: TableRow.LayoutParams =
             TableRow.LayoutParams(0, TableRow.LayoutParams.MATCH_PARENT, 1F)
 
-        var i: Int = 0
+        i = 0
         var j: Int
 
         while( i < numOfTimes ) {
@@ -85,8 +146,8 @@ class PersonalScheduleFragment: Fragment() {
             j = 0
             while( j < numOfDays ) {
                 //TableRow 안에 Button(cell) 생성
-                scheduleButtons[i][j].setBackgroundResource(R.drawable.bg_schedule_cell_btn)
-                scheduleButtons[i][j].layoutParams = cellPm
+                scheduleButtons[i][j].setBackgroundResource(R.drawable.bg_schedule_cell_btn2)
+//                scheduleButtons[i][j].layoutParams = cellPm
 
                 scheduleButtons[i][j].setOnClickListener(CellClickListener(scheduleData, i, j))
                 timeRows[i].addView(scheduleButtons[i][j])
