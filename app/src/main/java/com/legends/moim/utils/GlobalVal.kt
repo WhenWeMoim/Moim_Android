3910f647b5a50f826d043edd21c44a8ac2c759f0
vitaminwoo <58829994+vitaminwoo@users.noreply.github.com>
2022-06-26 16:46:23 +0900

    [feat] GroupScheduleFragment moimGroupColor algorithm

--- app/src/main/java/com/legends/moim/utils/GlobalVal.kt
+++ app/src/main/java/com/legends/moim/utils/GlobalVal.kt
@@ -1,9 +1,9 @@
 package com.legends.moim.utils
 
-const val CHOICE_LIKE = 1       //선호
-const val CHOICE_POSSIBLE = 2   //가능(Default)
-const val CHOICE_DISLIKE = 3    //비선호
-const val CHOICE_IMPOSSIBLE = 4 //불가능
+const val CHOICE_LIKE = 4       //선호
+const val CHOICE_POSSIBLE = 3   //가능(Default)
+const val CHOICE_DISLIKE = 2    //비선호
+const val CHOICE_IMPOSSIBLE = 1 //불가능
 
 
 const val FLAG_ACTIVITY_MAIN = 10001
